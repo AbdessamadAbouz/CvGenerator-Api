97de0f52b2173e4c6965aa43419453b4b956c845
Abdessamad Abouzaid <abdessamad.abouz@gmail.com>
2020-06-07 00:07:27 +0100

    Generate some responding as json

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,56 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Response;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected $status_code = 400;
+
+    // Getter and Setter for the status code
+    //
+    public function setStatusCode($status_code) {
+        $this->status_code = $status_code;
+        return $this;
+    }
+
+    public function getStatusCode() {
+        return $this->status_code;
+    }
+
+
+    // Generating respond in the followed structure
+    //
+    public function respond($data) {
+        return Response::json($data, $this->getStatusCode());
+    }
+
+    public function respondWithError($message, $error='')
+	{
+		return $this->respond([
+			'error' => [
+				'message'           => $message,
+				'status_code'       => $this->getStatusCode(),
+				'errors_validation' => $error
+			]
+		]);
+	}
+
+
+    public function notAuthorized($message = 'Not Found') {
+        return $this->setStatusCode(403)->respondWithError($message);
+    }
+
+    public function respondWithSuccess(array $data) {
+        return $this->setStatusCode(200)->respond($data);
+    }
+
+    public function ValidationError($error) {
+        return $this->setStatusCode(422)->respondWithError('Validation Error over the fields' , $error);
+    }
+
+
+    
 }
