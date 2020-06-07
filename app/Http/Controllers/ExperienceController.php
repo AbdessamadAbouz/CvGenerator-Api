b62b39e01af8d97dda95644ec2f55305a2916b87
Abdessamad Abouzaid <abdessamad.abouz@gmail.com>
2020-06-07 20:55:53 +0100

    Add important features of experiences

--- app/Http/Controllers/ExperienceController.php
+++ app/Http/Controllers/ExperienceController.php
@@ -21,21 +21,98 @@ class ExperienceController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $user = auth()->user();
+        if(! $user)
+            return $this->notAuthorized();
+
+        $validator = Validator::make($request->all(), [
+            'label' => 'required|string|max:100',
+            'societe' => 'required|string|max:100',
+            'description' => 'required|string|max:255',
+            'date_debut' => 'required|date',
+            'date_fin' => 'required|date',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->ValidationError($validator->errors());
+        }
+
+        $experience = $user->experiences()->create($request->only(
+            'label',
+            'societe',
+            'description',
+            'date_debut',
+            'date_fin'
+        ));
+
+        return $this->respondWithSuccess([
+            'message' => 'Experience added succesfully!!',
+            'experience' => $experience
+        ]);
     }
 
     public function show($id)
     {
-        //
+        $user = auth()->user();
+        if(! $user)
+            return $this->notAuthorized();
+
+        $experience = Experience::find($id);
+
+        return $this->respondWithSuccess([
+            'message' => 'Your experience',
+            'experience' => $experience
+        ]);
     }
 
     public function update(Request $request, $id)
     {
-        //
+        $user = auth()->user();
+        if(! $user)
+            return $this->notAuthorized();
+
+        $experience = Experience::find($id);
+        if(! $experience)
+            return $this->NotFound();
+        
+        $validator = Validator::make($request->all(), [
+            'label' => 'sometimes|string|max:100',
+            'societe' => 'sometimes|string|max:100',
+            'description' => 'sometimes|string|max:255',
+            'date_debut' => 'sometimes|date',
+            'date_fin' => 'sometimes|date',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->ValidationError($validator->errors());
+        }
+
+        $experience->update($request->only(
+            'label',
+            'societe',
+            'description',
+            'date_debut',
+            'date_fin'
+        ));
+
+        return $this->respondWithSuccess([
+            'message' => 'Experience added succesfully!!',
+            'experience' => $experience
+        ]);
     }
 
     public function destroy($id)
     {
-        //
+        $user = auth()->user();
+        if(! $user)
+            return $this->notAuthorized();
+
+        $experience = Experience::find($id);
+
+        $experience->delete();
+
+        return $this->respondWithSucces([
+            'message' => 'Destroyed succesfully'
+        ]);
     }
 }
